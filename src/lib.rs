54151fe9d5b7dc7f36b82eaf48e3a9e1aa20bced
Sérgio Carvalho <sergio.carvalho@sooma.com>
2026-05-08 10:46:44 +0100

    Implement ToFieldSet derive

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 mod to_empty_fieldset;
+mod to_field_set;
 
 use proc_macro::TokenStream;
 
@@ -6,4 +7,10 @@ use proc_macro::TokenStream;
 pub fn to_empty_fieldset_derive(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).unwrap();
     to_empty_fieldset::impl_to_empty_fieldset(&ast)
+}
+
+#[proc_macro_derive(ToFieldSet, attributes(abstract_form))]
+pub fn to_field_set_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+    to_field_set::impl_to_field_set(&ast)
 }
